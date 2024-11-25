b09d988086bd4c58d3c40d1765b6f43848391ff9
William Firmino <williams.an@hotmail.com>
2024-11-25 16:21:27 -0300

    Projeto Mobile 1

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+    MaterialApp(
+      home: Container(
+        color: Colors.black,
+        child: Text("Olá Mundo!"),
+        alignment: Alignment.topCenter,
+      ),
+    );
+  }
+}
